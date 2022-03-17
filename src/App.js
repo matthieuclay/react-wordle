165a0717aec525207a78e41179c55326712ca134
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-17 12:03:22 +0100

    :sparkles: setting up keyboard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,93 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
-import { boardDefault } from "./components/Words";
+import { boardDefault, generateWordSet } from "./components/Words";
 
 export const AppContext = createContext();
 
 const App = () => {
   const [board, setBoard] = useState(boardDefault);
+  const [currentAttempt, setCurrentAttempt] = useState({
+    attempt: 0,
+    letterPosition: 0,
+  });
+  const [wordSet, SetWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+
+  const correctWord = "RIGHT";
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      SetWordSet(words.wordSet);
+    });
+  }, []);
+
+  const onSelectLetter = (keyValue) => {
+    if (currentAttempt.letterPosition > 4) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyValue;
+    setBoard(newBoard);
+    setCurrentAttempt({
+      attempt: currentAttempt.attempt,
+      letterPosition: currentAttempt.letterPosition + 1,
+    });
+  };
+
+  const onDelete = () => {
+    if (currentAttempt.letterPosition === 0) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = "";
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currentAttempt,
+      letterPosition: currentAttempt.letterPosition - 1,
+    });
+  };
+
+  const onEnter = () => {
+    if (currentAttempt.letterPosition !== 5) return;
+
+    let currentWord = "";
+    for (let i = 0; i < 5; i++) {
+      currentWord += board[currentAttempt.attempt][i];
+    }
+
+    if (wordSet.has(currentWord.toLowerCase())) {
+      setCurrentAttempt({
+        attempt: currentAttempt.attempt + 1,
+        letterPosition: 0,
+      });
+    } else {
+      alert("Word not found");
+    }
+
+    if (currentWord === correctWord) {
+    }
+  };
+
   return (
     <div className="App">
       <nav>
-        <h1>Hello World!</h1>
+        <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currentAttempt,
+          setCurrentAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+          correctWord,
+          disabledLetters,
+          setDisabledLetters,
+        }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
     </div>
   );
---
+++ src/components/Key.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+const Key = ({ keyValue, bigKey, disabled }) => {
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+  const selectLetter = () => {
+    if (keyValue === "ENTER") {
+      onEnter();
+    } else if (keyValue === "DELETE") {
+      onDelete();
+    } else {
+      onSelectLetter(keyValue);
+    }
+  };
+
+  return (
+    <div
+      className="key"
+      id={bigKey ? "big" : disabled && "disabled"}
+      onClick={selectLetter}
+    >
+      {keyValue}
+    </div>
+  );
+};
+
+export default Key;
