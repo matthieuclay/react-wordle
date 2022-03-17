fa257b664e461c11b6df883b77677cd7029bbbf0
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-17 12:03:50 +0100

    :art: coloring letters

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,10 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AppContext } from "../App";
 
 const Letter = ({ letterPosition, attemptValue }) => {
-  const { board } = useContext(AppContext);
+  const { board, correctWord, currentAttempt, setDisabledLetters } =
+    useContext(AppContext);
   const letter = board[attemptValue][letterPosition];
-  return <div className="letter">{letter}</div>;
+
+  const correct = correctWord[letterPosition] === letter;
+  const almost = !correct && letter !== "" && correctWord.includes(letter);
+  const letterState =
+    currentAttempt.attempt > attemptValue &&
+    (correct ? "correct" : almost ? "almost" : "error");
+
+  useEffect(() => {
+    if (letter !== "" && !correct && !almost) {
+      setDisabledLetters((prev) => [...prev, letter]);
+    }
+  }, [currentAttempt.attempt]);
+  return (
+    <div className="letter" id={letterState ? letterState : ""}>
+      {letter}
+    </div>
+  );
 };
 
 export default Letter;
